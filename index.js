4b5fa60e003df571bd43c459cf771967efdfa79b
Ian <iantwelve@gmail.com>
2022-07-21 19:56:24 -0700

    made additional updates

--- index.js
+++ index.js
@@ -6,5 +6,22 @@ header.style.textAlign = "left"
 let dogImages = document.querySelectorAll(".dog-image")
 for (let i=0; i< dogImages.length; i++){
     dogImages[i].style.borderRadius = "50px"
-    
 }
+
+let footer = document.querySelector(".footer")
+footer.style.color = "purple"
+
+let dogNames = document.querySelectorAll(".dog-name")
+for (let i=0; i<dogNames.length;i++){
+    dogNames[i].style.color = "red"
+    if(dogNames[i].textContent === "Pip") {
+        dogNames[i].style.color = "Black"
+    }
+}
+
+
+
+
+
+
+
