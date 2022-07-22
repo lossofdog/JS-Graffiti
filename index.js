f7b9bd520643b346428ebcb32a046c66cd4dbddc
Ian <iantwelve@gmail.com>
2022-07-21 21:20:32 -0700

    added css

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ for (let i=0; i < dogDesc.length; i++){
     dogDesc[i].style.color = "purple"
 }
 
+let button = document.createElement('button')
+//add text to the button
+button.append('Click This Button')
+//update the button size
+button.style.fontSize='25px'
+//add the button to the page
+footer.append(button)
+//add event listener? first thing is the action. the second is what happens after
+button.addEventListener("click", function(){
+    //when the button is clicked get rid of the header
+    header.remove()
+
+})
 
 
 
